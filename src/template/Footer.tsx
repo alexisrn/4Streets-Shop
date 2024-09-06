f0ad30556ea40b9799e5d7c808f3dae94f2c0a7b
Raphael  Alexis do Nascimento <111790679+alexisrn@users.noreply.github.com>
2024-09-05 22:52:41 -0300

    remove link footer

--- src/template/Footer.tsx
+++ src/template/Footer.tsx
@@ -12,10 +12,10 @@ export default function Footer() {
 <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            
                
                 <span className={`text-4xl p-[12px] ${lobster.className}`}>4Streets</span>
-            </a>
+            
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" className="hover:underline me-4 md:me-6">Sobre Nós</a>
