09223b64aa79822fd529d76fabdeeef6ef64f8bd
Raphael  Alexis do Nascimento <111790679+alexisrn@users.noreply.github.com>
2024-10-03 23:46:59 -0300

    Update Footer.tsx

--- src/template/Footer.tsx
+++ src/template/Footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">4StreetS</a>. Todo direito reservado.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 4StreetS. Todo direito reservado.</span>
     </div>
 </footer>
 
